a83762fa0cc901df0ebfb69e6c05990e9479cf9f
NejlaSetkic <99647005+NejlaSetkic@users.noreply.github.com>
2024-12-10 15:31:41 +0100

    Update build-all-release-channels.js

--- scripts/rollup/build-all-release-channels.js
+++ scripts/rollup/build-all-release-channels.js
@@ -20,7 +20,8 @@ const {buildEverything} = require('./build-ghaction');
 
 // Runs the build script for both stable and experimental release channels,
 // by configuring an environment variable.
-
+// Runs the build script for both stable and experimental release channels,
+// by configuring an environment variable.
 const sha = String(spawnSync('git', ['rev-parse', 'HEAD']).stdout).slice(0, 8);
 
 let dateString = String(
